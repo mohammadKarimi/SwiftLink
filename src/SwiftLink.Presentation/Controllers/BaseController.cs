de6e044ea9dbdd71b20b7d1b146cebea9b16fe84
mohammadKarimi <mha.karimi@gmail.com>
2024-01-16 16:25:58 +0330

    Refactor ISender in BaseController.

--- src/SwiftLink.Presentation/Controllers/BaseController.cs
+++ src/SwiftLink.Presentation/Controllers/BaseController.cs
@@ -5,9 +5,7 @@ namespace SwiftLink.Presentation.Controllers;
 
 [Route("api/v{v:apiVersion}/[controller]/[action]")]
 [ApiController]
-public abstract class BaseController : Controller
+public abstract class BaseController(ISender sender) : Controller
 {
-    private ISender _mediatR;
-    protected ISender MediatR => _mediatR ??= HttpContext.RequestServices.GetRequiredService<ISender>();
-
+    protected readonly ISender MediatR = sender;
 }
--- src/SwiftLink.Presentation/Controllers/LinkController.cs
+++ src/SwiftLink.Presentation/Controllers/LinkController.cs
@@ -1,16 +1,16 @@
 ﻿using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
 using SwiftLink.Application.UseCases.Links.Commmands;
+using SwiftLink.Application.UseCases.Links.Queries.VisitShortCode;
 using SwiftLink.Presentation.Extensions;
 using SwiftLink.Presentation.Filters;
 
 namespace SwiftLink.Presentation.Controllers;
 
-[ApiVersion("1.0")]
 public class LinkController : BaseController
 {
     [HttpPost]
-    [ShortenEndpointFilter]
+    [ApiVersion("1.0")]
     public async Task<IActionResult> Shorten([FromBody] GenerateShortCodeCommand command, CancellationToken cancellationToken = default)
     {
         var response = await MediatR.Send(command, cancellationToken);
@@ -18,6 +18,18 @@ public class LinkController : BaseController
             return Ok(response.MapToProblemDetails());
 
         return Ok(response);
+    }
+
+    [HttpGet, Route("/x/{shortCode}")]
+    [ShortenEndpointFilter]
+    public async Task<IActionResult> Shorten(string shortCode, [FromQuery] string password, CancellationToken cancellationToken = default)
+    {
+        var visitLinkQuery = new VisitShortenLinkQuery(shortCode, password, "");
+        var response = await MediatR.Send(visitLinkQuery, cancellationToken);
+        if (response.IsFailure)
+            return Ok(response.MapToProblemDetails());
+
+        return Redirect(response.Data);
     }
 }
 

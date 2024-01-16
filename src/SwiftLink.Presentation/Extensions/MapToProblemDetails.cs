d09321c412465cccc0312e6db7d2252a0614ca16
mohammadKarimi <mha.karimi@gmail.com>
2024-01-16 11:02:48 +0330

    The Cyclomatic Complexity of this method is 11 which is greater than 10 authorized.

--- src/SwiftLink.Presentation/Extensions/MapToProblemDetails.cs
+++ src/SwiftLink.Presentation/Extensions/MapToProblemDetails.cs
@@ -6,8 +6,15 @@ namespace SwiftLink.Presentation.Extensions;
 public static class MapToProblemDetailsExtension
 {
     public static ProblemDetails MapToProblemDetails(this Result result)
-    {
-        static int GetStatusCode(ErrorType type)
+     => new()
+     {
+         Status = GetStatusCode(result.Error.Type),
+         Type = result.Error.Type.ToString(),
+         Title = GetTitle(result.Error.Type),
+         Detail = result.Error.Message
+     };
+
+    private static int GetStatusCode(ErrorType type)
             => type switch
             {
                 ErrorType.Validation => StatusCodes.Status400BadRequest,
@@ -17,22 +24,13 @@ public static class MapToProblemDetailsExtension
                 _ => StatusCodes.Status500InternalServerError
             };
 
-        static string GetTitle(ErrorType type)
-           => type switch
-           {
-               ErrorType.Validation => "Bad Request",
-               ErrorType.Failure => "Internal Server Error",
-               ErrorType.NotFound => "Not Found",
-               ErrorType.None => "Internal Server Error",
-               _ => "Internal Server Error"
-           };
-
-        return new()
-        {
-            Status = GetStatusCode(result.Error.Type),
-            Type = result.Error.Type.ToString(),
-            Title = GetTitle(result.Error.Type),
-            Detail = result.Error.Message
-        };
-    }
+    private static string GetTitle(ErrorType type)
+       => type switch
+       {
+           ErrorType.Validation => "Bad Request",
+           ErrorType.Failure => "Internal Server Error",
+           ErrorType.NotFound => "Not Found",
+           ErrorType.None => "Internal Server Error",
+           _ => "Internal Server Error"
+       };
 }

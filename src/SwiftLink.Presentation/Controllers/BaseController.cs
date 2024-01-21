e3577c650e15207f593452696c173c30db821a2b
mohammadKarimi <mha.karimi@gmail.com>
2024-01-21 14:25:21 +0330

    Add BaseController, And encapsulate MapToProblem in BaseController.

---
+++ src/SwiftLink.Presentation/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
+using SwiftLink.Presentation.Extensions;
+using SwiftLink.Shared;
+
+namespace SwiftLink.Presentation.Controllers;
+
+[ApiController]
+public abstract class BaseController : Controller
+{
+    protected IActionResult OK<T>(Result<T> response)
+    {
+        if (response.IsFailure)
+            return Ok(response.MapToProblemDetails());
+
+        return Ok(response);
+    }
+}
--- src/SwiftLink.Presentation/Controllers/LinkController.cs
+++ src/SwiftLink.Presentation/Controllers/LinkController.cs
@@ -1,40 +1,25 @@
-﻿using Asp.Versioning;
-using MediatR;
+﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using SwiftLink.Application.UseCases.Links.Commands;
 using SwiftLink.Application.UseCases.Links.Queries.VisitShortCode;
-using SwiftLink.Presentation.Extensions;
 using SwiftLink.Presentation.Filters;
 
 namespace SwiftLink.Presentation.Controllers;
 
-[ApiController]
-public class LinkController(ISender sender) : Controller
+public class LinkController(ISender sender) : BaseController
 {
     private readonly ISender _mediarR = sender;
 
     [HttpPost]
     [Route("api/v{v:apiVersion}/[controller]/[action]")]
     public async Task<IActionResult> Shorten([FromBody] GenerateShortCodeCommand command, CancellationToken cancellationToken = default)
-    {
-        var response = await _mediarR.Send(command, cancellationToken);
-        if (response.IsFailure)
-            return Ok(response.MapToProblemDetails());
+        => OK(await _mediarR.Send(command, cancellationToken));
 
-        return Ok(response);
-    }
-
-    [HttpGet, Route("/api/{shortCode}")]
+    [HttpGet, Route("/api/{shortCode}")] //TODO: this routing should be removed.
     [ShortenEndpointFilter]
     public async Task<IActionResult> Shorten(string shortCode, [FromQuery] string password, CancellationToken cancellationToken = default)
     {
         var visitLinkQuery = new VisitShortenLinkQuery(shortCode, password, "");
-        var response = await _mediarR.Send(visitLinkQuery, cancellationToken);
-        if (response.IsFailure)
-            return Ok(response.MapToProblemDetails());
-
-        return Ok(response.Data);
+        return OK(await _mediarR.Send(visitLinkQuery, cancellationToken));
     }
 }
-
-

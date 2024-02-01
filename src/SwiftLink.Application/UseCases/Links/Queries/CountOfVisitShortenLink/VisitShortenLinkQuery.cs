19727007c87f81cb5df669b89e495a683dfc38de
mohammadKarimi <mha.karimi@gmail.com>
2024-02-01 21:35:52 +0330

    Add Count of Visit for specific LinkId.

---
+++ src/SwiftLink.Application/UseCases/Links/Queries/CountOfVisitShortenLink/VisitShortenLinkQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace SwiftLink.Application.UseCases.Links.Queries.VisitShortenLink;
+
+public record CountVisitShortenLinkQuery : IRequest<Result<string>>
+{
+    public int LinkId { get; set; }
+}
---
+++ src/SwiftLink.Application/UseCases/Links/Queries/CountOfVisitShortenLink/VisitShortenLinkQueryHandler.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+using MediatR;
+using SwiftLink.Application.Common;
+using SwiftLink.Application.Common.Interfaces;
+using SwiftLink.Application.Notifications;
+
+namespace SwiftLink.Application.UseCases.Links.Queries.VisitShortenLink;
+
+public class VisitShortenLinkQueryHandler(IApplicationDbContext dbContext,
+                                          ICacheProvider cacheProvider,
+                                          IMediator mediator)
+    : IRequestHandler<VisitShortenLinkQuery, Result<string>>
+{
+    private readonly IApplicationDbContext _dbContext = dbContext;
+    private readonly ICacheProvider _cache = cacheProvider;
+    private readonly IMediator _mediator = mediator;
+
+    public async Task<Result<string>> Handle(VisitShortenLinkQuery request, CancellationToken cancellationToken)
+    {
+        Link link;
+        var cacheResult = await _cache.Get(request.ShortCode);
+        if (!string.IsNullOrEmpty(cacheResult))
+            link = JsonSerializer.Deserialize<Link>(cacheResult);
+        else
+        {
+            link = await _dbContext.Set<Link>().FirstOrDefaultAsync(x => x.ShortCode == request.ShortCode, cancellationToken);
+            if (link is null)
+                return Result.Failure<string>(LinkMessages.LinkIsNotFound);
+            await _cache.Set(request.ShortCode, JsonSerializer.Serialize(link));
+        }
+
+        if (link.IsBanned)
+            return Result.Failure<string>(LinkMessages.LinkIsBanned);
+
+        if (link.ExpirationDate <= DateTime.Now)
+            return Result.Failure<string>(LinkMessages.LinkIsExpired);
+
+        if (link.Password is not null)
+        {
+            if (request.Password is null)
+            {
+                return Result.Failure<string>(LinkMessages.PasswordIsNotSent);
+            }
+            else if (request.Password.Hash(link.OriginalUrl) != link.Password)
+                return Result.Failure<string>(LinkMessages.InvalidPassword);
+        }
+
+        await _mediator.Publish(new VisitLinkNotification
+        {
+            LinkId = link.Id,
+            ClientMetaData = request.ClientMetaData
+        }, cancellationToken);
+
+        return Result.Success(link.OriginalUrl);
+    }
+}

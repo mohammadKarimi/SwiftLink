1c2e387488b8111d700daf7777ea86b38e54c6f7
mohammadKarimi <mha.karimi@gmail.com>
2024-02-03 13:49:55 +0330

    Change Settings.

--- src/SwiftLink.Shared/AppSettings.cs
+++ src/SwiftLink.Shared/AppSettings.cs
@@ -6,9 +6,11 @@ public record AppSettings
 {
     public static string ConfigurationSectionName => "AppSettings";
 
-    [Range(1, 60)] public byte DefaultExpirationTimeInDays { get; set; }
+    [Range(1, 60)] 
+    public byte DefaultExpirationTimeInDays { get; set; }
 
-    [Url] public string DefaultUrlOnNotFound { get; set; }
+    [Url] 
+    public string DefaultUrlOnNotFound { get; set; }
 
     public Redis Redis { get; set; }
 }
@@ -17,5 +19,6 @@ public record Redis
 {
     public int SlidingExpirationHour { get; set; }
 
-    [Url] public string RedisCacheUrl { get; set; }
+    [Url] 
+    public string RedisCacheUrl { get; set; }
 }

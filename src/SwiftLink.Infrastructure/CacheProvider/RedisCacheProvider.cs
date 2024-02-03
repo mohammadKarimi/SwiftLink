8d0a0c3c28021108c4e6147e865fb260e190ec07
mohammadKarimi <mha.karimi@gmail.com>
2024-02-03 16:27:09 +0330

    Add Polly to Circuit Breaker when we want to add a short code to redis.

--- src/SwiftLink.Infrastructure/CacheProvider/RedisCacheProvider.cs
+++ src/SwiftLink.Infrastructure/CacheProvider/RedisCacheProvider.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.Extensions.Caching.Distributed;
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Options;
+using Polly;
+using Polly.CircuitBreaker;
+using StackExchange.Redis;
 using System.Text;
 
 namespace SwiftLink.Infrastructure.CacheProvider;
@@ -7,6 +12,10 @@ namespace SwiftLink.Infrastructure.CacheProvider;
 public class RedisCacheService(IDistributedCache cache, IOptions<AppSettings> options)
     : ICacheProvider
 {
+
+    private readonly AsyncCircuitBreakerPolicy<bool> circuitBreakerPolicy = Policy<bool>.HandleResult(false)
+                                                                                        .CircuitBreakerAsync(2, TimeSpan.FromSeconds(30));
+
     private readonly IDistributedCache _cache = cache;
     private readonly AppSettings _options = options.Value;
 
@@ -18,25 +27,37 @@ public class RedisCacheService(IDistributedCache cache, IOptions<AppSettings> op
 
     public async Task<bool> Set(string key, string value, DateTime expirationDate)
     {
-        try
-        {
-            DistributedCacheEntryOptions cacheEntryOptions = new()
-            {
-                SlidingExpiration = TimeSpan.FromHours(_options.Redis.SlidingExpirationHour),
-                AbsoluteExpiration = expirationDate,
-            };
-
-            var dataToCache = Encoding.UTF8.GetBytes(value);
-            await _cache.SetAsync(key, dataToCache, cacheEntryOptions);
-            return true;
-        }
-        catch (Exception e)
-        {
-            //Log e
+        if (circuitBreakerPolicy.CircuitState is CircuitState.Open)
             return false;
-        }
+        return await circuitBreakerPolicy.ExecuteAsync(async () =>
+            {
+                try
+                {
+                    DistributedCacheEntryOptions cacheEntryOptions = new()
+                    {
+                        SlidingExpiration = TimeSpan.FromHours(_options.Redis.SlidingExpirationHour),
+                        AbsoluteExpiration = expirationDate,
+                    };
+                    var dataToCache = Encoding.UTF8.GetBytes(value);
+                    await _cache.SetAsync(key, dataToCache, cacheEntryOptions);
+                }
+                catch (RedisConnectionException)
+                {
+                    return false;
+                }
+                return true;
+            });
     }
 
     public async Task<string> Get(string key)
-        => await _cache.GetStringAsync(key);
+    {
+        try
+        {
+            return await _cache.GetStringAsync(key);
+        }
+        catch
+        {
+            return null;
+        }
+    }
 }

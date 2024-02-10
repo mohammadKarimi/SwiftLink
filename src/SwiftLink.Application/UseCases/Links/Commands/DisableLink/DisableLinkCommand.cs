6737acf05e2f83697f735ce3a12aa7ac119a0da4
Parham Darvishi <darvishiparham14@gmail.com>
2024-02-10 22:39:48 +0330

    Add new api for disable link

---
+++ src/SwiftLink.Application/UseCases/Links/Commands/DisableLink/DisableLinkCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace SwiftLink.Application.UseCases.Links.Commands.DisableLink;
+
+public class DisableLinkCommand : IRequest<Result<bool>>
+{
+    public int Id { get; set; }
+}

73fcd29235bac22f796c0cd22270528e9dcb58e7
mohammadKarimi <mha.karimi@gmail.com>
2024-02-12 17:41:27 +0330

    Remove Unused Files.

--- src/SwiftLink.Presentation/Program.cs
+++ src/SwiftLink.Presentation/Program.cs
@@ -74,9 +74,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 {
     if (app.Environment.IsDevelopment())
-    {
         await app.InitializeDatabaseAsync();
-    }
 
     app.UseExceptionHandler();
     app.UseAuthorization();

1754c5e5b3fa1789ed3a759d637637d3d1918a41
mohammadKarimi <mha.karimi@gmail.com>
2024-03-01 14:51:35 +0330

    Change Versions.

---
+++ src/SwiftLink.Domain/Events/LinkVisitedEvent.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using SwiftLink.Domain.Entities;
+
+namespace SwiftLink.Domain.Events;
+
+public class LinkVisitedEvent(LinkVisit linkVisit) : INotification
+{
+    public LinkVisit LinkVisit { get; } = linkVisit;
+}

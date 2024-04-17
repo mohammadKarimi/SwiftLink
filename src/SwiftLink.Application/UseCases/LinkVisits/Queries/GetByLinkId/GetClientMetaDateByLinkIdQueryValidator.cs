0cd1ee5bcb7ada2d73f26dc47335dbebbae5ee02
mohammadKarimi <mha.karimi@gmail.com>
2024-04-17 22:09:13 +0330

    remove unused line

--- src/SwiftLink.Application/UseCases/LinkVisits/Queries/GetByLinkId/GetClientMetaDateByLinkIdQueryValidator.cs
+++ src/SwiftLink.Application/UseCases/LinkVisits/Queries/GetByLinkId/GetClientMetaDateByLinkIdQueryValidator.cs
@@ -5,7 +5,6 @@ namespace SwiftLink.Application.UseCases.LinkVisits.Queries;
 public class GetClientMetaDateByLinkIdQueryValidator : AbstractValidator<GetClientMetaDataByLinkIdQuery>
 {
     public GetClientMetaDateByLinkIdQueryValidator()
-
     {
         RuleFor(x => x.LinkId)
             .NotNull().WithMessage(LinkVisitMessages.LinkIdMustBeSent.Message);
